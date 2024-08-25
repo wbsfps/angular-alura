ca48e8a1cd4caab055f2f4419ceba0dc9ca92c86
William Santana <williamandrade1058@gmail.com>
2024-08-25 16:10:50 -0300

    event binding

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -19,4 +19,11 @@ export class CriarPensamentoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  criarPensamento() {
+    alert('Novo pensamento criado!');
+  }
+
+  cancelar() {
+    alert('Pensamento cancelado!');
+  }
 }
